915a8e24abdd7110ea5cf3d4d6ccf36ff237958a
kevin willian <tste@outlook.com.br>
2017-11-21 17:26:36 -0200

    busca binaria com vetor

--- trabalhos/buscabinaria.c
+++ trabalhos/buscabinaria.c
@@ -35,34 +35,37 @@ void inserir(int *vet){
     }
     bubble(vet);
 }
-// x => chave | *vet => vetor ordenado | e => limite inferior (esquerda) | d => limite superior (direita)
-//int x, int v[], int e, int d
 void bbin(int *vet){
     int x,e,d;
 
     printf("Procurar o numero:");
     scanf("%d",&x);
-    e=vet[0];
-    d=vet[TAM-1];
+    e=0;
+    d=TAM-1;
     pbin(x,vet,e,d);
 }
-int pbin (int x, int *vet, int e, int d){
+// x => chave | *vet => vetor ordenado | e => limite inferior (esquerda) | d => limite superior (direita)
+void pbin (int x, int *vet, int e, int d){
  int meio = (e + d)/2;
- if (vet[meio] == x)
-    return meio;
- if (e >= d)
-    return -1; // não encontrado
+ if (vet[meio] == x){
+    printf("Valor encontrado na posicao %d\n",meio);
+    return;
+}
+ if (e >= d){
+    printf("Valor não encontrado!\n");
+    return;
+}
  else
      if (vet[meio] < x)
-        return pbin(x,vet,meio+1,d);
+        pbin(x,vet,meio+1,d);
      else
-        return pbin(x,vet,e,meio-1);
+        pbin(x,vet,e,meio-1);
 }
 
 int main(){
-    int vet[TAM]={10,9,8,7,6,5,4,3,2,1};
+    int vet[TAM];
 
-    //inserir(vet);
-    bubble(vet);
+    inserir(vet);
+    display(vet);
     bbin(vet);
 }
